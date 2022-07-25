d6c35c6a7e59f8bc3b786cfed7dc2a68bb299fa9
Kathy Smith <109811392+kathysmith-appdev4ever@users.noreply.github.com>
2022-07-25 13:51:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,6 +104,7 @@ if streamlit.button('Get Fruit Load List'):
 # -- Don't run anything past here while we troubleshoot --
 streamlit.stop()
 
+# =================================================================================================
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
@@ -112,3 +113,18 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 # It has a control flow issue.  This piece of code is executing everytime we interact with the page.
 # It should on execute when we want to add a fruit.
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# =================================================================================================
+
+# =================================================================================================
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+       return "Thanks for adding " + new_fruit
+      
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+# =================================================================================================
