366a9a053f9bd1afca958395e80f16fec22e4515
Kathy Smith <109811392+kathysmith-appdev4ever@users.noreply.github.com>
2022-07-25 13:05:18 -0400

    Update streamlit_app.py
    Added Control Flow with "try" statement and Error Handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,38 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# =================================================================================================
+# New Code with Control Flow and Error Handling
 # New Section to display frutiyvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Take the JSON version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Output it to the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+# =================================================================================================
+
+# =================================================================================================
+# Old Code with No Control Flow and No Error Handling
+# New Section to display frutiyvice api response
+# streamlit.header("Fruityvice Fruit Advice!")
+# streamlit.write('The user entered ', fruit_choice)
 
 # import requests -- Moved to the top
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Take the JSON version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
+# =================================================================================================
 
 # -- Don't run anything past here while we troubleshoot --
 streamlit.stop()
